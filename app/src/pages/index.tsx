82d37a2c8a9d74707cb446a19f6a92e860582b3a
Kayo Ronald <kayoronald4321@gmail.com>
2023-09-08 21:24:03 +0000

    feat: colocando o getStaticProps

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { GetStaticProps} from 'next'
+import { GetStaticProps } from 'next'
 import { Heading, Center } from "@chakra-ui/react";
 import getConfig from 'next/config'
 import axios from 'axios';
 const { publicRuntimeConfig } = getConfig()
 
-export default function Home() {
+export default function Home({data}: any) {
+  console.log(data)
   return (
     <Center>
       <Heading>Olá, Mundo!</Heading>
@@ -12,8 +13,23 @@ export default function Home() {
   )
 }
 
+interface NoteProps {
+  id: string;
+  title: string;
+  description: string;
+}
+interface IndexPageProps {
+  data: NoteProps[];
+}
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { data } = await axios.get(publicRuntimeConfig.apiURL)
-  console.log(data)
-  return { props: { data } }
+  const { data } = await axios.get<IndexPageProps>(`${publicRuntimeConfig.apiURL}/note`)
+  return {
+    props: {
+      data:  data.data.map((note: NoteProps) => ({
+        id: note.id,
+        title: note.title,
+        description: note.description
+      }))
+    }
+  }
 }
