2f6a01d8ff9515513161db6681030ffa907bb536
Kayo Ronald <kayoronald4321@gmail.com>
2023-10-18 01:00:59 +0000

    Pending changes exported from your codespace

---
+++ app/src/components/Note/Contentlist.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+import StackGrid from "react-stack-grid"
+
+export const Contentlist = () => {
+  return(
+    <Box minH={"50vh"}>
+       <StackGrid columnWidth={330}>
+
+       </StackGrid>
+    </Box>
+  )
+}
