5a04eda9790fc15572a20c940ee854235e0ba64a
KayoRonald <kayoronald4321@gmail.com>
2023-11-06 20:49:43 -0300

    fix> bug unique item

--- server/models/Note.go
+++ server/models/Note.go
@@ -9,14 +9,14 @@ import (
 
 type Note struct {
 	gorm.Model
-	ID        			string `gorm:"type:uuid;primaryKey" json:"id"`
-	Title      			string    `gorm:"not null" json:"title"`
-	Description     string    `gorm:"unique" json:"description"`
-	CreatedAt 			*time.Time
-	UpdatedAt 			*time.Time
+	ID          string `gorm:"type:uuid;primaryKey" json:"id"`
+	Title       string `gorm:"not null" json:"title"`
+	Description string `gorm:"not null" json:"description"`
+	CreatedAt   *time.Time
+	UpdatedAt   *time.Time
 }
 
-func (u *Note) BeforeSave(tx *gorm.DB) (err error){
+func (u *Note) BeforeSave(tx *gorm.DB) (err error) {
 	u.ID = uuid.New().String()
 	return
 }
